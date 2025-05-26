51c363e83b251da79d4432ce961f28ce88603cde
Santiago <santiago@tonta.mx>
2025-05-25 22:46:45 -0600

    Bug Fix

--- main.py
+++ main.py
@@ -270,6 +270,6 @@ async def get_cubbies():
         
         result.append(cubby_info)
 
-    result.sort(key=lambda x: x["cubbyid"])  # Sort by cubbyid
+    result.sort(key=lambda x: x.get("cubbyid", 0))
     return result
 
